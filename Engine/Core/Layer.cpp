58ce3d66fd304b88e509ac84d2c1fa93c83b8cc2
pengzzzccc <153042982+pengzzzccc@users.noreply.github.com>
2026-06-05 01:45:32 +1000

    Update Layer.cpp

--- Engine/Core/Layer.cpp
+++ Engine/Core/Layer.cpp
@@ -7,4 +7,4 @@ void Layer::OnAttach() {}
 void Layer::OnDetach() {}
 void Layer::OnUpdate(float deltaTime) {}
 void Layer::OnRender() {}
-bool Layer::OnEvent() {} // need event
+bool Layer::OnEvent() { return true } // need event
